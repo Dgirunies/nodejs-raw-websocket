44f1ed7bdf8a3364aaa68e26074b76b10a7f48be
sgednethgirao <edneth@sheetgo.com>
2022-11-29 01:47:01 +0100

    fix: add error handling to prevent server crash

--- server.mjs
+++ server.mjs
@@ -6,3 +6,15 @@ createServer((request, response) => {
     response.end("Server is alive")
 })
 .listen(PORT, () => console.log("Server listening to", PORT))
+
+// error handling to keep the server on
+;
+[
+    'uncaughtException',
+    'unhandledRejection'
+].forEach(event =>
+    process.on(event, (err) => {
+        console.error(`Something gone wrong: ${event}, msg: ${err.stack || err}`)
+    }))
+
+process.on("uncaughtException")
